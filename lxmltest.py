0cc2f4713fbe04fa772281ddb42fca0420258976
Kendall Jackson <kendallkj00@gmail.com>
2019-07-29 15:03:47 -0400

    Added new dataset, changed loop break

--- lxmltest.py
+++ lxmltest.py
@@ -14,19 +14,19 @@ import pandas as pd
 negatives = ['sentry.wixpress.com', 'example.com', 'domain.com', 'address.com', 'xxx.xxx', 'email.com', 'yourdomain.com']
 
 # Reads website column, initializes counter variable
-df = pd.read_csv('./Argo.csv')
+df = pd.read_csv('./mergedORL.csv')
 urls = list(dict.fromkeys(df['website']))
 counter = 0
 final_list = []
 print_list = []
 
- # Set Headers 
+ # Set Headers
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 
 def get_email(url):
-    
+
     # Filtering Function
     def filter_func(x):
         ind = x.find('@')+1
@@ -36,33 +36,33 @@ def get_email(url):
 
     # Get HTML, regexp match, filter out bad emails
     try:
-        
+
         site = requests.get(url, verify=True, headers=headers, timeout=(2, 2)).content.decode()
         possible_emails = re.findall(r'[A-Za-z0-9._%+-]{3,}@[a-z]{3,}\.[a-z]{2,}(?:\.[a-z]{2,})?', site)
         print('Fetched Web Page.\n')
         res = list(set(filter(filter_func,possible_emails)))
 
-      
+
     except:
         print('Web Page Not Found. Deleting...')
         return []
-    
-    
+
+
     if(not res):
         print('No Emails Found. Deleting...')
         return []
 
-   
+
     else:
-        
+
         print('Emails:\n')
         print(res)
-        
+
         return res
 
     return []
 
-    
+
 if __name__ == "__main__":
     for link in urls:
         print(link)
@@ -70,11 +70,10 @@ if __name__ == "__main__":
         if(email):
             for mail in [elem.lower() for elem in email]:
                 final_list.append(mail)
-            
+
             counter += len(email)
-            
-        if(counter >= 101):
-            break
+
+        
         print('------------------------')
         print(str(counter) + ' Email(s) found so far.')
         print('------------------------')
@@ -91,7 +90,3 @@ if __name__ == "__main__":
             writer.writerow(data)
 
     print('File written!')
-            
-
-
-
